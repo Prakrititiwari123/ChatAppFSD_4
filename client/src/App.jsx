21ce7de237f856d7c9ee678a4b1a1bb380425c2f
Prakrititiwari123 <anuragtiwaribadkhara@gmail.com>
2026-02-05 08:46:32 +0530

    App.jsx is modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
@@ -7,14 +7,14 @@ import Register from './pages/Register';
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
